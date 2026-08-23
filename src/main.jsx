bcba616ccf94357e1eb2711daecf96afad1e8ba2
zakariarupto-alt <zakaria.rupto@gmail.com>
2026-08-23 23:37:09 +0600

    Wire universal command center

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import GlobalCommand from './GlobalCommand.jsx'
 import './styles.css'
 import './polish.css'
+import './command.css'
 
 const basename=(import.meta.env.BASE_URL||'/').replace(/\/$/,'')||'/'
 
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
       <App />
+      <GlobalCommand />
     </BrowserRouter>
   </React.StrictMode>,
 )

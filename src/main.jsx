7263057b1bddb8c1026747abb9a662adc725c721
zakariarupto-alt <zakaria.rupto@gmail.com>
2026-08-23 23:50:03 +0600

    Integrate Command Center Pro modules

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import GlobalCommand from './GlobalCommand.jsx'
+import CommandCenterPro from './CommandCenterPro.jsx'
 import './styles.css'
 import './polish.css'
 import './command.css'
 import './advanced.css'
+import './command-center-pro.css'
 
 const basename=(import.meta.env.BASE_URL||'/').replace(/\/$/,'')||'/'
 
@@ -15,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter basename={basename}>
       <App />
       <GlobalCommand />
+      <CommandCenterPro />
     </BrowserRouter>
   </React.StrictMode>,
 )
